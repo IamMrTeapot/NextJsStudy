7be068e71e67fc3d1e2495b0ebbbfeb673434ac9
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-10 18:27:45 +0700

    there are some problems about styled-components lib, so i just leave it.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import type { AppProps } from "next/app";
+
+const theme = {
+  colors: {
+    primary: "#355C7D",
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
