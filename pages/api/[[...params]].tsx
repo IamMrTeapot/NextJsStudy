277d7c23bbf9d908d33f094349ab17205b42247c
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-10 17:22:48 +0700

    catches all routes

---
+++ pages/api/[[...params]].tsx
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const params = req.query.params;
+  console.log(params);
+  res.status(200).json(params);
+};
