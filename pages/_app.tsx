9682f9be1e4624a97c02a94db6ebbec38daf4c43
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-10 18:50:52 +0700

    Head component and priority

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/layout.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -19,6 +20,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <title>Nextjs</title>
+        <meta name="description" content="This framework is too Hard TT" />
+        {/* the meta tag in index has higher priority */}
+      </Head>
+
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Footer from "../components/Footer";
 import styles from "../styles/About.module.scss";
 
 function About() {
   return (
     <div>
+      <Head>
+        <title>About Nextjs</title>
+        <meta name="description" content="I can change this" />
+      </Head>
       <h2 className={styles.highlightscss}>About Page</h2>
       <button className="btn btn-primary">Primary</button>
     </div>
