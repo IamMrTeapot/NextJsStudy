641746957d9b2d043203c0141dd1266bb0df9fff
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-10 19:32:05 +0700

    add auto redirect to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/contact',
+        destination: '/',
+        permanent: false
+      },
+      {
+        source: '/old-blog/:id',
+        destination: '/new-blog/:id',
+        permanent: true
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
